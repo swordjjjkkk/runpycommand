3198d313e0dac0f370281c1f6cbe194fd719d33d
swordjjjkkk <675540285@qq.com>
2021-11-03 17:40:15 +0800

    init vscode plugin

---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	let disposable = vscode.commands.registerCommand('extpy.runpycommand', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInputBox(
+			{
+                placeHolder:'请输入函数名' // 在输入框内的提示信息
+            }).then(function(msg){
+			let activeEditor = vscode.window.activeTextEditor;
+
+			let text=activeEditor?.document.getText(activeEditor.selection);
+
+            console.log("用户输入："+msg);
+            console.log("用户选择："+text);
+			
+
+        });
+	});
+
+	let disposable2 = vscode.commands.registerCommand('extpy.openpyfile', () => {
+	});
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
