329b7f3655074c8f36b273108eecea75244d4d02
swordjjjkkk <675540285@qq.com>
2021-11-04 16:40:51 +0800

    add register command

--- src/extension.ts
+++ src/extension.ts
@@ -5,73 +5,84 @@ import * as vscode from 'vscode';
 import { PythonShell } from 'python-shell';
 
 // your extension is activated the very first time the command is executed
+function runPyCommand(vsctx: vscode.ExtensionContext,text:string,command:string)
+{
+
+	let activeEditor = vscode.window.activeTextEditor;
+	let options={
+		// mode:'text',
+		// pythonPath:'python',
+		pythonOptions:['-u'],
+		scriptPath:__dirname,
+		args:[text,command!]
+	};
+	try
+	{
+		PythonShell.run("script.py",options,function(err,results){
+			if(err)
+			{
+				vscode.window.showInformationMessage("RunPyCommand: script.py command crashed , please check your code!! ");
+				console.log(err);
+			}
+			let res=JSON.parse(results![0]);
+			if(res["res"]===0)
+			{
+				console.log(res);
+				activeEditor!.edit(editBuilder => {
+					if (activeEditor!.selection.isEmpty)
+					{
+						editBuilder.insert(activeEditor!.selection.active,res["data"]);
+					}
+					else
+					{
+						editBuilder.replace(activeEditor!.selection, res["data"]);
+					}
+				});
+			}
+			else if(res["res"]===8000)
+			{
+				console.log(res);
+				for(let i=0;i<res["data"].length;i++)
+				{
+					let tmp = vscode.commands.registerCommand('extpy.'+res["data"][i], () => {
+						let activeEditor = vscode.window.activeTextEditor;
+						let text=activeEditor!.document.getText(activeEditor!.selection);
+						runPyCommand(vsctx,text,res["data"][i]);
+					});
+					vsctx.subscriptions.push(tmp);
+				}
+
+			}
+			else
+			{
+				console.log("python run result wrong!!");
+				vscode.window.showInformationMessage("RunPyCommand: script.py command did't print res:0 ");
+			}
+		});
+	}
+	catch(err)
+	{
+		vscode.window.showInformationMessage("RunPyCommand:got some error.but it's ok! don't be afraid! ");
+	}
+}
 export function activate(context: vscode.ExtensionContext) {
-	
+	runPyCommand(context,"","getAllCommand");
 	let disposable = vscode.commands.registerCommand('extpy.runpycommand', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInputBox(
 		{
-			placeHolder:'请输入函数名' // 在输入框内的提示信息
+			placeHolder:'Input Command' // 在输入框内的提示信息
 		}).then(function(msg){
-
 			let activeEditor = vscode.window.activeTextEditor;
 			let text=activeEditor!.document.getText(activeEditor!.selection);
-
-            console.log("用户输入："+msg);
-            console.log("用户选择："+text);
-
-			let options={
-				// mode:'text',
-				// pythonPath:'python',
-				pythonOptions:['-u'],
-				scriptPath:__dirname,
-				args:[text,msg!]
-			};
-			PythonShell.run("script.py",options,function(err,results){
-				if(err)
-				{
-					console.log(err);
-				}
-				let res=JSON.parse(results![0]);
-				if(res["res"]===0)
-				{
-					console.log(res);
-					activeEditor!.edit(editBuilder => {
-						if (activeEditor!.selection.isEmpty)
-						{
-							editBuilder.insert(activeEditor!.selection.active,res["data"]);
-						}
-						else
-						{
-							editBuilder.replace(activeEditor!.selection, res["data"]);
-						}
-					});
-				}
-				else
-				{
-					console.log("python run result wrong!!");
-				}
-			});
-			
-			// const child=execFile("python",[__dirname+"\\script.py",msg!,text!],(error: any,stdout: any,stderr: any)=>{
-			// 	console.log(error);
-			// 	console.log(stdout);
-			// 	console.log(stderr);
-			// 	activeEditor!.edit(editBuilder => {
-			// 			editBuilder.replace(activeEditor!.selection, stdout);
-			// 	});
-			// });
-			
-
+			runPyCommand(context,text,msg!);
         });
 	});
 
 	let disposable2 = vscode.commands.registerCommand('extpy.openpyfile', () => {
+		vscode.window.showTextDocument(vscode.Uri.file(__dirname+"/script.py"));
 	});
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposable2);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
